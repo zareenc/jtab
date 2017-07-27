5739061a74538f14f7e9ca79c759f64390d86951
Zareen Choudhury <zareenc@mit.edu>
2017-07-27 16:36:42 -0700

    Comments to background.js

--- jtab/background.js
+++ jtab/background.js
@@ -1,3 +1,9 @@
-chrome.browserAction.onClicked.addListener(pinCallback);
-
+// When new tab is created, add tab info to localStorage
 chrome.tabs.onCreated.addListener(newTabCallback);
+
+// When navigate to new URL, delete duplicate tabs
+
+// When tab is deleted, remove tab info from localStorage
+
+// Toggle pinning and unpinning with Omnibox icon
+chrome.browserAction.onClicked.addListener(pinCallback);
