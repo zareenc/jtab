a9ec0907083a4fcef1699061bcb7de73b2abba75
Zareen Choudhury <zareenc@mit.edu>
2017-07-27 22:44:42 -0700

    Move option helper functions into helpers.js

--- jtab/background.js
+++ jtab/background.js
@@ -1,33 +1,3 @@
-/* Gets all options from local storage
- * :return: associative array of options
- */
-function getOptions() {
-  if ("options" in localStorage) {
-    return JSON.parse(localStorage["options"])
-  } else {
-    return {}
-  }
-}
-
-/* Save option in local storage
- *  :param option string: option name/key
- *  :param value: option value
- */
-function setOption(option, value) {
-  options = getOptions()
-  options[option] = value
-  localStorage["options"] = JSON.stringify(options)
-}
-
-/* Gets an option from local storage
- *  :param option string: option name/key
- *  :return: option value or undefined, if does not exist
- */
-function getOption(option) {
-  options = getOptions()
-  return options[option]
-}
-
 // When new tab is created, add tab info to localStorage
 chrome.tabs.onCreated.addListener(newTabCallback);
 
