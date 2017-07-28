82afe9efca7ba5b6fbf58208425e218d11c7abd2
Charles Wan <yty.charles@gmail.com>
2017-07-27 19:59:11 -0700

    Add local deps and option saving to local storage

--- jtab/background.js
+++ jtab/background.js
@@ -1,3 +1,29 @@
+/* Gets all options from local storage
+ * :return: associative array of options
+ */
+function getOptions() {
+  if ("options" in localStorage) {
+    return JSON.parse(localStorage["options"])
+  } else {
+    return {}
+  }
+}
 
-getTabUrls()
-console.log('hello')
+/* Save option in local storage
+ *  :param option string: option name/key
+ *  :param value: option value
+ */
+function setOption(option, value) {
+  options = getOptions()
+  options[option] = value
+  localStorage["options"] = JSON.stringify(options)
+}
+
+/* Gets an option from local storage
+ *  :param option string: option name/key
+ *  :return: option value or undefined, if does not exist
+ */
+function getOption(option) {
+  options = getOptions()
+  return options[option]
+}
